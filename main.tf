ef197156723905835e922dce8cad0b047d049563
Callum Innes <me@cinnes.dev>
2024-07-29 02:09:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,3 @@ provider "google" {
 data "google_client_config" "google_config" {
 
 }
-
-resource "google_project_service" "cloud_resource_manager" {
-  project = data.google_client_config.google_config.id
-  service = "cloudresourcemanager.googleapis.com"
-}

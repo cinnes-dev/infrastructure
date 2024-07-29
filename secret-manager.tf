f0dc5e4906d12df7b35978dd8640ad7b12e6225b
Callum Innes <me@cinnes.dev>
2024-07-29 04:42:21 +0100

    Update secret-manager.tf

--- secret-manager.tf
+++ secret-manager.tf
@@ -1,3 +1,8 @@
+resource "google_project_service" "artifactregistry_api" {
+  project = google_project.default.id
+  service = "secretmanager.googleapis.com"
+}
+
 resource "google_secret_manager_secret" "health-ingest-key" {
   secret_id = "health-ingest-key"
   project = google_project.default.id

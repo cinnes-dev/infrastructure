d24750ff0616ee44c099e8fb8ece2fc84af211aa
Callum Innes <me@cinnes.dev>
2024-07-29 02:02:43 +0100

    Enable cloud resource manager API

--- project.tf
+++ project.tf
@@ -1,3 +1,9 @@
+resource "google_project_service" "cloud_resource_manager" {
+  project = google_project.default.id
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+
 import {
   id = "cinnes-dev-id"
   to = google_project.default
